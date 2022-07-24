aaf96417571e9b6d45d46d61d545eae5c6b0bd53
przemograf <60073682+przemograf@users.noreply.github.com>
2022-07-24 21:02:42 +0200

    Update app.py

--- streamlit-prettymapp/app.py
+++ streamlit-prettymapp/app.py
@@ -186,8 +186,8 @@ with st.spinner("Creating map... (may take up to a minute)"):
     st.pyplot(fig, pad_inches=0, bbox_inches="tight", transparent=True, dpi=600)
 
 
-  # svg_string = plt_to_svg(fig)
-  # html = svg_to_html(svg_string)
+  svg_string = plt_to_svg(fig)
+  html = svg_to_html(svg_string)
   # st.write("")
   # fname = slugify(address)
   # img_format = st.selectbox("Download image as", ["svg", "png", "jpg"], index=0)
@@ -197,7 +197,7 @@ with st.spinner("Creating map... (may take up to a minute)"):
   #     import io
   #     data = io.BytesIO()
   #     fig.savefig(data, pad_inches=0, bbox_inches="tight", transparent=True)
-  # st.download_button(label="Download image", data=data, file_name=f"{fname}.{img_format}")
+  st.download_button(label="Download image", data=data, file_name=f"{fname}.{img_format}")
 
 st.markdown("---")
 st.write(
